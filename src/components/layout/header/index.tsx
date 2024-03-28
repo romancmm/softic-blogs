e4b91e820a33deffc0388e375997832ed2c6d1ba
romancmm <romancmm@gmail.com>
2024-03-29 02:55:57 +0600

    add nav bar in header section

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,14 +1,15 @@
 
 import { Container } from '@/components/common'
 import React from 'react'
+import { Navbar } from './navbar'
+import { siteConfig } from '@/config/site'
 
 export const Header = () => {
+    // const items = siteConfig?. 
     return (
         <header className='sticky top-0 bg-white bg-opacity-25 backdrop-blur text-gray-900 py-2'>
             <Container variant={"fluid"}>
-                <div className="bg-white">
-                    Header
-                </div>
+                <Navbar  />
             </Container>
         </header>
     )
---
+++ src/components/layout/header/navbar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Navbar: any = () => {
+    return (
+        <div>Navbar</div>
+    )
+}
