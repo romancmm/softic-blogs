b9ea67a626756ae303d266e3ba302d0fe852b851
romancmm <romancmm@gmail.com>
2024-03-29 02:28:55 +0600

    Makeing custom component for container

---
+++ src/components/common/Container/Container.tsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const containerVariants = cva("container", {
+    variants: {
+        variant: {
+            default: "max-w-6xl",
+            fluid: "max-w-full",
+        },
+    },
+    defaultVariants: {
+        variant: "default",
+    },
+})
+
+export interface ContainerProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof containerVariants> { }
+
+const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
+    ({ className, variant, ...props }) => {
+        const Comp: React.ComponentType<React.HTMLAttributes<HTMLDivElement>> | any = "div"
+        return (
+            <Comp
+                className={cn(containerVariants({ variant, className }))}
+                {...props}
+            />
+        )
+    }
+)
+Container.displayName = "Container"
+
+export { containerVariants }
+export default Container 
--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,7 +1,15 @@
+
+import { Container } from '@/components/common'
 import React from 'react'
 
 export const Header = () => {
     return (
-        <header className='sticky top-0 bg-white bg-opacity-25 backdrop-blur text-gray-900 '>Header</header>
+        <header className='sticky top-0 bg-white bg-opacity-25 backdrop-blur text-gray-900 py-2'>
+            <Container>
+                <div className="bg-white">
+                    Header
+                </div>
+            </Container>
+        </header>
     )
 }
