475d80de7152dc771f6233888fd072e44fc0bf12
romancmm <romancmm@gmail.com>
2024-03-29 01:10:43 +0600

    initialize main header

---
+++ src/components/layout/header/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header className='sticky top-0 bg-white bg-opacity-25 backdrop-blur text-gray-900 '>Header</header>
+    )
+}
