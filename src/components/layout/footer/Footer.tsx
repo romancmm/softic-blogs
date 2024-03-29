a6cc2e73f46c6978a4f34bf9c5528bb861d08480
romancmm <romancmm@gmail.com>
2024-03-29 17:11:45 +0600

    add social links in footer section

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -2,12 +2,22 @@ import React from 'react'
 import { Container } from '@/components/common';
 import { siteConfig } from '@/config/site';
 import Link from 'next/link';
+import Icon from '@/components/icons';
+import { LucideIcon } from 'lucide-react';
 
 const Footer = () => {
     return (
         <footer className='bg-background py-24'>
             <Container variant={"fluid"}>
                 <div className="flex flex-col gap-10 justify-center items-center">
+                    <div className="flex gap-4">
+                        {siteConfig?.socialLinks?.map((data: { title: string, href: string, colorCode: string, icon: string | LucideIcon | any }) =>
+                            <Link href={data?.href} target="_blank" key={data?.title} className='p-3 bg-muted hover:shadow-md rounded-full'>
+                                <Icon name={data?.icon} size={20} color={`#fff` || data?.colorCode} />
+                            </Link>
+                        )}
+
+                    </div>
                     <div className="">
                         <ul className='flex gap-6'>
                             {
