7a94f98157883520143001fa1a487330d5421088
romancmm <romancmm@gmail.com>
2024-03-30 16:12:03 +0600

    setup antd and implemnt on blog card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { siteConfig } from "@/config/site";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
+          <AntdRegistry>
+            {children}
+          </AntdRegistry>
         </ThemeProvider>
       </body>
     </html>
--- src/components/common/BlogCard/BlogCard.tsx
+++ src/components/common/BlogCard/BlogCard.tsx
@@ -1,3 +1,4 @@
+import { Card } from 'antd'
 import Image from 'next/image'
 import React from 'react'
 
@@ -7,24 +8,25 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ data }) => {
     return (
-        <div key={data?.id} className="
-           max-w-full h-full
-            bg-white dark:bg-gray-700 rounded-md md:rounded-xl overflow-hidden shadow-md hover:shadow-lg group 
-        ">
-            <div className="w-full overflow-hidden rounded-md z-0">
-                <Image
-                    src={'/images/bg.jpeg'}
-                    alt=''
-                    height={300}
-                    width={500}
-                    className='object-cover group-hover:scale-110 transition-500ms duration-500 delay-500 ease-in-out'
-                />
+        <Card
+            hoverable
+        >
+            <div key={data?.id} className="group flex flex-col gap-5">
+                <div className="w-full overflow-hidden rounded-md z-0">
+                    <Image
+                        src={'/images/bg.jpeg'}
+                        alt=''
+                        height={300}
+                        width={500}
+                        className='object-cover group-hover:scale-110 transition-500ms duration-500 delay-500 ease-in-out'
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h2 className='text-lg font-semibold text-muted leading-snug capitalize'>{data?.title}</h2>
+                    <p className='font-normal text-sm text-gray-600 leading-relaxed capitalize'>{data?.body}</p>
+                </div>
             </div>
-            <div className="p-6 flex flex-col gap-2">
-                <h2 className='text-xl font-bold drop-shadow-lg leading-tight capitalize'>{data?.title}</h2>
-                <p className='font-light text-sm'>{data?.body}</p>
-            </div>
-        </div>
+        </Card>
     )
 }
 
