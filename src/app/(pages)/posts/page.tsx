cfbedde4c500637cf1ac4e71dd82fcbb26f51ae6
romancmm <romancmm@gmail.com>
2024-03-31 17:49:04 +0600

    add posts page

--- src/app/(pages)/posts/page.tsx
+++ src/app/(pages)/posts/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { Container, LoaderTwo, Section } from '@/components/common'
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect, useMemo } from 'react'
 import { setPosts, setLoading, setError, mergeData } from '@/store/postsSlice';
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { useGetCommentsQuery, useGetPhotosQuery, useGetPostsQuery, useGetUsersQuery } from '@/store/apiSlices';
-import ContentLeft from './ContentLeft';
+import ContentLeft from '@/components/home/MainContent/ContentLeft';
 
 
-function MainContent() {
+export default function BlogPage() {
     const dispatch = useAppDispatch();
     const { data: posts, isLoading, error } = useGetPostsQuery();
     const { data: comments } = useGetCommentsQuery();
@@ -38,21 +38,22 @@ function MainContent() {
 
     if (isLoading || loading) return <LoaderTwo />;
     if (error || postsError) return <div>Error: {postsError}</div>;
+
     return (
-        <Section variant={"default"} bg={"light"}>
-            <Container>
-                <div className="grid grid-cols-3 items-start gap-2 md:gap-6">
-                    <div className="col-span-full md:col-span-2 relative ">
-                        <ContentLeft items={loadedPosts} />
+        <>
+            <Section variant={"default"} bg={"light"}>
+                <Container>
+                    <div className="grid grid-cols-3 items-start gap-2 md:gap-6">
+                        <div className="col-span-full md:col-span-2 relative ">
+                            <ContentLeft items={loadedPosts} />
+                        </div>
+                        <div className="col-span-full md:col-span-1 flex justify-start flex-col text-gray-700 border border-gray-100 rounded-lg p-6 sticky top-28 shadow bg-foreground">
+                            {/* <BannerRight blogs={data} /> */}
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero iusto dolores, obcaecati doloremque ad consequatur repudiandae voluptates adipisci maiores facere earum error, beatae harum a, deleniti delectus esse accusantium.
+                        </div>
                     </div>
-                    <div className="col-span-full md:col-span-1 flex justify-start flex-col text-gray-700 border border-gray-100 rounded-lg p-6 sticky top-28 shadow bg-foreground">
-                        {/* <BannerRight blogs={data} /> */}
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vero iusto dolores, obcaecati doloremque ad consequatur repudiandae voluptates adipisci maiores facere earum error, beatae harum a, deleniti delectus esse accusantium.
-                    </div>
-                </div>
-            </Container>
-        </Section>
+                </Container>
+            </Section>
+        </>
     )
 }
-
-export default MainContent
--- src/components/home/MainContent/ContentLeft.tsx
+++ src/components/home/MainContent/ContentLeft.tsx
@@ -1,46 +1,15 @@
 'use client'
-import { BlogCard, HorizontalCard } from '@/components/common'
-import { useEffect, useMemo } from "react";
-import { setPosts, setLoading, setError, mergeData } from '@/store/postsSlice';
-import { LoaderTwo } from "@/components/common";
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { useGetCommentsQuery, useGetPhotosQuery, useGetPostsQuery, useGetUsersQuery } from '@/store/apiSlices';
+import { BlogCard, HorizontalCard, LoaderTwo } from '@/components/common'
+interface ContentLeftProps {
+    items: any[]
+}
 
-const ContentLeft = () => {
-    const dispatch = useAppDispatch();
-    const { data: posts, isLoading, error } = useGetPostsQuery();
-    const { data: comments } = useGetCommentsQuery();
-    const { data: users } = useGetUsersQuery();
-    const { data: photos } = useGetPhotosQuery();
+const ContentLeft: React.FC<ContentLeftProps> = ({ items }) => {
 
-    const mergedPosts = useMemo(() => {
-        if (posts && comments && users && photos) {
-            return mergeData(posts, comments, users, photos).slice(10, 30);
-        }
-        return [];
-    }, [posts, comments, users, photos]);
-
-    useEffect(() => {
-        dispatch(setLoading());
-
-        if (!posts || !comments || !users || !photos) {
-            dispatch(setError('Failed to fetch data'));
-            return;
-        }
-
-        if (mergedPosts.length > 0) {
-            dispatch(setPosts(mergedPosts));
-        }
-    }, [dispatch, mergedPosts, posts, comments, users, photos]);
-
-    const { posts: loadedPosts, loading, error: postsError } = useAppSelector((state) => state.posts);
-
-    if (isLoading || loading) return <LoaderTwo />;
-    if (error || postsError) return <div>Error: {postsError}</div>;
 
     return (
         <div className="grid grid-cols-none md:grid-cols-2 grid-rows-4 gap-2 md:gap-4">
-            {loadedPosts?.map((data: any, i: number) =>
+            {items?.map((data: any, i: number) =>
                 data?.id % 5 === 0
                     ? <div className="row-span-3" key={i}>
                         <BlogCard data={data} />
