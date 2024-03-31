625431be34eb0bf6234062a7940fbcdf5acda284
romancmm <romancmm@gmail.com>
2024-03-31 13:49:59 +0600

    merge comment, photo and user with post

--- src/components/home/MainContent/ContentLeft.tsx
+++ src/components/home/MainContent/ContentLeft.tsx
@@ -1,32 +1,43 @@
 'use client'
 import { BlogCard, HorizontalCard } from '@/components/common'
-import { useEffect } from "react";
-import { useGetPostsQuery } from '@/store/apiSlices';
+import { useEffect, useMemo } from "react";
 import { setPosts, setLoading, setError } from '@/store/postsSlice';
 import { LoaderTwo } from "@/components/common";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { Photo, Post, User, Comment } from '@/types';
+import { useGetCommentsQuery, useGetPhotosQuery, useGetPostsQuery, useGetUsersQuery } from '@/store/apiSlices';
 
 const ContentLeft = () => {
     const dispatch = useAppDispatch();
-    const { data: posts, isLoading, error } = useGetPostsQuery();
+    const { data: posts } = useGetPostsQuery();
+    const { data: comments } = useGetCommentsQuery();
+    const { data: users } = useGetUsersQuery();
+    const { data: photos } = useGetPhotosQuery();
+
+    const mergedPosts = useMemo(() => {
+        if (posts && comments && users && photos) {
+            return mergeData(posts, comments, users, photos).slice(0, 20);
+        }
+        return [];
+    }, [posts, comments, users, photos]);
 
     useEffect(() => {
-        if (isLoading) {
-            dispatch(setLoading());
+        dispatch(setLoading());
+
+        if (!posts || !comments || !users || !photos) {
+            dispatch(setError('Failed to fetch data'));
+            return;
         }
-        if (error) {
-            dispatch(setError((error as any).message));
+
+        if (mergedPosts.length > 0) {
+            dispatch(setPosts(mergedPosts));
         }
-        if (posts) {
-            dispatch(setPosts(posts.slice(0, 20))); // Load only first 20 posts
-        }
-    }, [isLoading, posts, error, dispatch]);
+    }, [dispatch, mergedPosts, posts, comments, users, photos]);
 
     const { posts: loadedPosts, loading, error: postsError } = useAppSelector((state) => state.posts);
 
-    if (loading || isLoading) return <LoaderTwo />
-    if (postsError || error) return <div>Error: {postsError || error}</div>
-
+    if (loading) return <LoaderTwo />;
+    if (postsError) return <div>Error: {postsError}</div>;
     return (
         <div className="grid grid-cols-2 grid-rows-4 flex-wrap gap-4">
             {loadedPosts?.map((data: any, i: number) =>
@@ -39,5 +50,23 @@ const ContentLeft = () => {
         </div>
     )
 }
+
+
+// Helper function to merge data on IDs
+export const mergeData = (
+    posts: Post[],
+    comments: Comment[],
+    users: User[],
+    photos: Photo[],
+) => {
+    return posts.map((post) => ({
+        ...post,
+        numberOfComments: comments.filter((comment) => comment.postId === post.id)?.length,
+        user: users.find((user) => user.id === post.userId)?.name,
+        photo: photos.find((photo) => photo.id === post.id)?.url,
+    }))
+}
+
+
 
 export default ContentLeft
